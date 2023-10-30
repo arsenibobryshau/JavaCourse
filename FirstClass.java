1e2a8f7b8e89d5deb9e61bc880a7c1887b26c7e5
Arseni Bobryshau <arsenibobryshau@gmail.com>
2023-10-30 23:29:58 +0100

    First try

--- FirstClass.java
+++ FirstClass.java
@@ -1,5 +1,5 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("First try working with Git by my self.");
+        System.out.println("First try working with Git by my self. Here is going to be all my journey through the Java course.");
     }
 }
